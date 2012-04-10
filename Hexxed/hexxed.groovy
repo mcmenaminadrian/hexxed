3b46f9b41b8926db92d8a71bdc1d1a3c54659f26
Adrian McMenamin <adrianmcmenamin@gmail.com>
2012-04-10 13:17:37 +0100

    starting

---
+++ Hexxed/hexxed.groovy
@@ -0,0 +1,97 @@
+package Hexxed
+
+import java.nio.channels.FileChannel
+
+class HexFileHandler {
+
+	def fileName
+	def fileHandle
+	def fileOffset
+	def displayEngine
+	def blockSize
+	def fileChan
+	def randomFile
+	
+	HexFileHandler(def le, def be, def bits, def offset,
+		def bSize, def blocks, def name)
+	{
+		displayEngine = new HexDisplayState(le, be, bits, bSize, blocks)
+		fileName = name
+		fileOffset = offset
+		blockSize = bSize
+		
+		try {
+			randomFile = new RandomAccessFile(name, "rw") 
+			fileChan = randomFile.getChannel()
+			fileChan.position(fileOffset)
+		}
+		catch(e) {
+			println "Unable to open file $fileName, exception $e"
+		}
+		
+	
+		def displayLines = new HexDisplay(displayEngine, fileChan)
+		for (i in 0 .. 16) {
+			displayLines.showLine()
+		}
+
+		fileChan.close()
+		randomFile.close()		
+	}
+	
+}
+
+def hexCli = new CliBuilder
+	(usage: 'hexxed [options] <file to edit>')
+	
+	hexCli.o(longOpt: 'offset', args: 1,
+		'offset in file - default 0')
+	hexCli.b(longOpt: 'block',
+		'use block:offset address output - default is linear address')
+	hexCli.s(longOpt: 'blocksize', args: 1,
+		'size of block if block:offset addressing used - default is 0x200')
+	hexCli.w(longOpt: 'width', args: 1,
+		'width (in bits, 8 - 64 bits) of output data - default is 8 bits')
+	hexCli.le(longOpt: 'littleendian',
+		'interpret data as little endian - default is cpu endianness')
+	hexCli.be(longOpt: 'bigendian',
+		'interpret data as big endian - default is cpu endianness');
+	hexCli.u(longOpt: 'usage', 'show this information')
+	
+	def hexParse = hexCli.parse(args)
+	if (hexParse.u || args.size() == 0) {
+		hexCli.usage()
+	} else {
+		def bits = 8
+		def le = false
+		def be = false
+		def bs = 512
+		def blocks = false
+		def offset = 0
+		
+		if (hexParse.le)
+			le = true
+		else if (hexParse.be)
+			be = true
+			
+		if (hexParse.b) {
+			blocks = true
+			if (hexParse.s)
+				bs = Integer.parseInt(hexParse.s)
+		}
+		
+		if (hexParse.w){
+			bits = Integer.parseInt(hexParse.w)
+			bits = bits & 0xF8
+			if (bits == 0)
+				bits = 8
+			else if (bits > 64)
+				bits = 64
+		}
+		
+		if (hexParse.o)
+			offset = Integer.parseInt(hexParse.o)
+		
+		def hexFileHandler = new HexFileHandler(le, be, bits, offset, bs,
+			blocks, args[args.size() - 1])	
+	}
